5835e358088968e6ac2518fcf924a59a2dd559e4
Javi Roberts <jrobertsotero@gmail.com>
2017-06-13 18:05:14 -0300

    working calculator

--- js/main.js
+++ js/main.js
@@ -4,5 +4,9 @@
 */
 
 (function() {
-    document.getElementById("hello").innerHTML = "Hola mundo!";
+    $('#btn-suma').on('click', function(){
+        var first = parseFloat($('#first').val());
+        var last = parseFloat($('#last').val());
+        $('#result').val(first + last);
+    });
 })();
